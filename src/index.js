a0a1f7eb00905b72694443f97ec075f689b7e7b5
Mark Murphy <murphy.mark@northeastern.edu>
2022-03-24 18:16:42 -0400

    Add second dropdown

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { registerDropdown } from './dropdown';
 const main = function () {
   const vegetableDropdown = document.getElementById('vegetable-dropdown');
   registerDropdown(vegetableDropdown);
+
+  const flowerDropdown = document.getElementById('flower-dropdown');
+  registerDropdown(flowerDropdown);
 };
 
 main();
